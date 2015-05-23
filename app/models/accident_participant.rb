d098eba137c02b255c59f8fd8042d494ddfe1e49
Yelizaveta Yaskilevich <yelizaveta.yaskilevich@gmail.com>
2015-05-23 18:45:48 +0300

    AccidentParticipant validation

--- app/models/accident_participant.rb
+++ app/models/accident_participant.rb
@@ -2,4 +2,8 @@ class AccidentParticipant < ActiveRecord::Base
   belongs_to :accident
   belongs_to :person
   belongs_to :vehicle
+
+  validates :accident, presence: true
+  validates :person, presence: true
+  validates :vehicle, presence: true
 end
