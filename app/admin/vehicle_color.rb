0f8592f8e83cbd62ffa4ba2e4104c5302bb230ca
Yelizaveta Yaskilevich <yelizaveta.yaskilevich@gmail.com>
2015-05-24 19:18:27 +0300

    vehicle color pages refactoring

--- app/admin/vehicle_color.rb
+++ app/admin/vehicle_color.rb
@@ -1,4 +1,30 @@
 ActiveAdmin.register VehicleColor do
-  menu label: 'ТС - Цвета', parent: 'Справочник', priority: 5
   config.sort_order = 'name_asc'
+
+  menu label: 'ТС - Цвета', parent: 'Справочник', priority: 5
+
+  filter :name, label: 'Наименование'
+
+  index title: 'Цвета транспортных средств' do
+    column 'Наименование', sortable: 'name' do |vehicle_color|
+      link_to vehicle_color.name, admin_vehicle_color_path(vehicle_color)
+    end
+  end
+
+  permit_params :name
+
+  form do |f|
+    inputs 'Цвет транспортного средств' do
+      input :name, label: 'Наименование'
+    end
+    actions
+  end
+
+  show do
+    panel 'Цвет транспортного средств' do
+      attributes_table_for vehicle_color do
+        row('Наименование'){ vehicle_color.name }
+      end
+    end
+  end
 end
