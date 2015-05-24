0ac90f922d13ce1cd3bce85f18563748a2a11cc6
Yelizaveta Yaskilevich <yelizaveta.yaskilevich@gmail.com>
2015-05-24 19:15:04 +0300

    vehicle category pages refactoring

--- app/admin/vehicle_category.rb
+++ app/admin/vehicle_category.rb
@@ -1,4 +1,33 @@
 ActiveAdmin.register VehicleCategory do
-  menu label: 'ТС - Категории', parent: 'Справочник', priority: 5
   config.sort_order = 'name_asc'
+
+  menu label: 'ТС - Категории', parent: 'Справочник', priority: 5
+
+  filter :name, label: 'Наименование'
+
+  index title: 'Категории транспортных средств' do
+    column 'Наименование', sortable: 'name' do |vehicle_category|
+      link_to vehicle_category.name, admin_vehicle_category_path(vehicle_category)
+    end
+    column 'Описание', :description, sortable: false
+  end
+
+  permit_params :name, :description
+
+  form do |f|
+    inputs 'Категория транспортных средств' do
+      input :name, label: 'Наименование'
+      input :description, label: 'Описание'
+    end
+    actions
+  end
+
+  show do
+    panel 'Категория транспортных средств' do
+      attributes_table_for vehicle_category do
+        row('Наименование'){ vehicle_category.name }
+        row('Описание'){ vehicle_category.description }
+      end
+    end
+  end
 end
